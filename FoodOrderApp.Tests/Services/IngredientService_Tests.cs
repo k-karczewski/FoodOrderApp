c66d97b31a71d0dc6dc4aa5d6a5f5c4f3afb90ab
Konrad Karczewski <kkarczewski94@gmail.com>
2020-06-02 18:53:49 +0200

    Recreated IngredientService unit tests using Mocks

--- FoodOrderApp.Tests/Services/IngredientService_Tests.cs
+++ FoodOrderApp.Tests/Services/IngredientService_Tests.cs
@@ -1,14 +1,20 @@
-﻿using FoodOrderApp.Interfaces.Services;
+﻿using Autofac.Extras.Moq;
+using FoodOrderApp.Data.Repositories;
+using FoodOrderApp.Interfaces.Repositories;
+using FoodOrderApp.Interfaces.Services;
 using FoodOrderApp.Interfaces.Services.ServiceResults;
 using FoodOrderApp.Interfaces.UnitOfWork;
 using FoodOrderApp.Models.PizzaModels;
 using FoodOrderApp.Models.PizzaModels.PriceModels;
 using FoodOrderApp.Services;
-using FoodOrderApp.Tests._Fakes.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,28 +23,17 @@ namespace FoodOrderApp.Tests.Services
     [TestClass]
     public class IngredientService_Tests
     {
-        private readonly IUnitOfWork _unitOfWorkFake;
-        private readonly IIngredientService _service;
-
-        public int Id;
-        List<IngredientModel> ingredients;
-
-        public IngredientService_Tests()
-        {
-            _unitOfWorkFake = new UnitOfWork_Fake();
-            _service = new IngredientService(_unitOfWorkFake);
-        }
+        List<IngredientModel> expectedIngredients;
+        Mock<IUnitOfWork> uowMock;
+        Mock<IFoodOrderRepository<IngredientModel>> repoMock;
 
         [TestInitialize]
         public void Initialize()
         {
-            Id = 1;
-            InitializeRepo();
-        }
+            this.uowMock = new Mock<IUnitOfWork>();
+            this.repoMock = new Mock<IFoodOrderRepository<IngredientModel>>();
 
-        private void InitializeRepo()
-        {
-            ingredients = new List<IngredientModel>
+            this.expectedIngredients = new List<IngredientModel>
             {
                 new IngredientModel
                 {
@@ -81,37 +76,72 @@ namespace FoodOrderApp.Tests.Services
                     }
                 }
             };
-
-            _unitOfWorkFake.Ingredients.CreateAsync(ingredients[0]);
-            _unitOfWorkFake.Ingredients.CreateAsync(ingredients[1]);
         }
 
         [TestMethod]
         public async Task GetIngredientByCorrectId()
         {
-            IServiceResult<IngredientModel> result = await _service.GetByIdAsync(Id);
+            int idOfIngredient = 1;
+
+            // mock getByExpression method from repository            
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(), 
+                                                       It.IsAny<Func<IQueryable<IngredientModel>, IIncludableQueryable<IngredientModel, object>>>())).
+                                                       Returns(GetFakeIngredientById(idOfIngredient));
+
+            // mock Ingredients property from uow
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            // create new instance of IngredientService using mocked UnitOfWork
+            IIngredientService service = new IngredientService(uowMock.Object);
+
+            IServiceResult<IngredientModel> result = await service.GetByIdAsync(idOfIngredient);
 
             Assert.AreEqual(ResultType.Correct, result.Result);
-            Assert.AreEqual(ingredients[0].Name, result.ReturnedObject.Name);
+            Assert.AreEqual(expectedIngredients[0].Id, result.ReturnedObject.Id);
+            Assert.AreEqual(expectedIngredients[0].Name, result.ReturnedObject.Name); 
         }
 
         [TestMethod]
         public async Task GetIngredientByIncorrectId()
         {
-            IServiceResult<IngredientModel> result = await _service.GetByIdAsync(Id-1000);
+            // incorrect id
+            int idOfIngredient = -1000;
+
+            // mock getByExpression method from repository            
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(),
+                                                       It.IsAny<Func<IQueryable<IngredientModel>, IIncludableQueryable<IngredientModel, object>>>())).
+                                                       Returns(GetFakeIngredientById(idOfIngredient));
+
+            // mock Ingredients property from uow
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            // create new instance of IngredientService using mocked UnitOfWork
+            IIngredientService service = new IngredientService(uowMock.Object);
+
+            IServiceResult<IngredientModel> result = await service.GetByIdAsync(idOfIngredient);
 
             Assert.AreEqual(ResultType.Error, result.Result);
+            Assert.IsNull(result.ReturnedObject);
             Assert.AreEqual(1, result.Errors.Count);
-            Assert.AreEqual($"Object with id: {Id-1000} has not been found", result.Errors.ElementAt(0));
+            Assert.AreEqual($"Object with id: {idOfIngredient} has not been found", result.Errors.First());
         }
 
         [TestMethod]
         public async Task GetAllIngredients()
         {
-            IServiceResult<List<IngredientModel>> result = await _service.GetAsync();
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(),
+                                                        It.IsAny<Func<IQueryable<IngredientModel>, IIncludableQueryable<IngredientModel, object>>>())).
+                                                        Returns(GetFakeIngredients());
 
-            Assert.AreEqual(ResultType.Correct, result.Result);
-            Assert.AreEqual(ingredients.Count, result.ReturnedObject.Count);
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            IIngredientService ingredientService = new IngredientService(uowMock.Object);
+
+            IServiceResult<List<IngredientModel>> getAllResult = await ingredientService.GetAsync();
+
+            Assert.AreEqual(ResultType.Correct, getAllResult.Result);
+            Assert.IsNotNull(getAllResult.ReturnedObject);
+            Assert.AreEqual(expectedIngredients.Count, getAllResult.ReturnedObject.Count);
         }
 
         [TestMethod]
@@ -123,10 +153,19 @@ namespace FoodOrderApp.Tests.Services
                 Name = "Tomato"
             };
 
-            IServiceResult<IngredientModel> result = await _service.CreateAsync(newIngredient);
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(), null)).Returns(GetFakeIngredientByName(newIngredient.Name));
+            repoMock.Setup(x => x.CreateAsync(newIngredient)).Returns(Task.FromResult(true));
 
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            IIngredientService ingredientService = new IngredientService(uowMock.Object);
+
+            IServiceResult<IngredientModel> result = await ingredientService.CreateAsync(newIngredient);
+            
             Assert.AreEqual(ResultType.Created, result.Result);
-            Assert.AreEqual(newIngredient, result.ReturnedObject);
+            Assert.IsNotNull(result.ReturnedObject);
+            Assert.AreEqual(newIngredient.Id, result.ReturnedObject.Id);
+            Assert.AreEqual(newIngredient.Name, result.ReturnedObject.Name);
         }
 
         [TestMethod]
@@ -135,49 +174,214 @@ namespace FoodOrderApp.Tests.Services
             IngredientModel newIngredient = new IngredientModel
             {
                 Id = 3,
-                Name = "Tomato"
+                Name = "Mushrooms"
             };
 
-            await _service.CreateAsync(newIngredient);
-            IServiceResult<IngredientModel>  result = await _service.CreateAsync(newIngredient);
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(), null)).Returns(GetFakeIngredientByName(newIngredient.Name));
+            repoMock.Setup(x => x.CreateAsync(newIngredient)).Returns(Task.FromResult(true));
+
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            IIngredientService ingredientService = new IngredientService(uowMock.Object);
+
+            await ingredientService.CreateAsync(newIngredient);
+            IServiceResult<IngredientModel> result = await ingredientService.CreateAsync(newIngredient);
 
             Assert.AreEqual(ResultType.Error, result.Result);
+            Assert.IsNull(result.ReturnedObject);
             Assert.AreEqual(1, result.Errors.Count);
+            Assert.AreEqual($"Ingredient with name {newIngredient.Name} already exists in database", result.Errors.First());
         }
 
         [TestMethod]
         public async Task DeleteExistingIngredient()
         {
-            IServiceResult result = await _service.DeleteAsync(ingredients[1].Id);
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(), null)).Returns(GetFakeIngredientByName(expectedIngredients[0].Name));
+            repoMock.Setup(x => x.DeleteAsync(expectedIngredients[0])).Returns(Task.FromResult(true));
 
-            Assert.AreEqual(ResultType.Deleted, result.Result);
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            IIngredientService ingredientService = new IngredientService(uowMock.Object);
+
+            IServiceResult deleteResult = await ingredientService.DeleteAsync(expectedIngredients[0].Id);
+
+            Assert.AreEqual(ResultType.Deleted, deleteResult.Result);
+            Assert.IsNull(deleteResult.Errors);
         }
 
         [TestMethod]
         public async Task DeleteNotExistingIngredient()
         {
-            await _service.DeleteAsync(ingredients[1].Id);
-            IServiceResult result = await _service.DeleteAsync(ingredients[1].Id);
+            IngredientModel newIngredient = new IngredientModel
+            {
+                Id = 3,
+                Name = "Tomato"
+            };
 
-            Assert.AreEqual(ResultType.Error, result.Result);
-            Assert.AreEqual(1, result.Errors.Count);
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(), null)).Returns(GetFakeIngredientByName(newIngredient.Name));
+
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+
+            IIngredientService ingredientService = new IngredientService(uowMock.Object);
+
+            IServiceResult deleteResult = await ingredientService.DeleteAsync(newIngredient.Id);
+
+            Assert.AreEqual(ResultType.Error, deleteResult.Result);
+            Assert.IsNotNull(deleteResult.Errors);
+            Assert.AreEqual($"Ingredient with id {newIngredient.Id} was not found in database", deleteResult.Errors.First());
         }
 
         [TestMethod]
         public async Task UpdatePrice()
         {
+            int idOfIngredient = 1;
+            PizzaModel pizza = (await GetFakePizzas()).First();
             IngredientPriceModel newPrice = new IngredientPriceModel
             {
                 Id = 1,
-                Size = 0,
+                Size = SizeEnum.Small,
                 Price = 5.50M
             };
 
-            IServiceResult<IngredientModel> result = await _service.UpdatePriceAsync(newPrice, 1);
+            Mock<IFoodOrderRepository<PizzaModel>> pizzaRepoMock = new Mock<IFoodOrderRepository<PizzaModel>>();
+
+            repoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<IngredientModel, bool>>>(),
+                                           It.IsAny<Func<IQueryable<IngredientModel>, IIncludableQueryable<IngredientModel, object>>>())).
+                                           Returns(GetFakeIngredientById(idOfIngredient));
+
+            repoMock.Setup(x => x.UpdateAsync(expectedIngredients[idOfIngredient])).Returns(Task.FromResult(true));
+  
+            pizzaRepoMock.Setup(x => x.GetByExpressionAsync(It.IsAny<Expression<Func<PizzaModel, bool>>>(),
+                                           It.IsAny<Func<IQueryable<PizzaModel>, IIncludableQueryable<PizzaModel, object>>>())).
+                                           Returns(GetFakePizzas());
+          
+            pizzaRepoMock.Setup(x => x.UpdateAsync(pizza)).Returns(Task.FromResult(true));
+
+
+            uowMock.Setup(x => x.Ingredients).Returns(repoMock.Object);
+            uowMock.Setup(x => x.SaveChangesAsync());
+            uowMock.Setup(x => x.Pizzas).Returns(pizzaRepoMock.Object);
+
+            IIngredientService ingredientService = new IngredientService(uowMock.Object);
+
+            IServiceResult<IngredientModel> result = await ingredientService.UpdatePriceAsync(newPrice, idOfIngredient);
+            
+            uowMock.Verify(x => x.SaveChangesAsync(), Times.Exactly(2));
 
             Assert.AreEqual(ResultType.Edited, result.Result);
-            Assert.AreEqual(newPrice.Price, result.ReturnedObject.Prices.First(x => x.Size == newPrice.Size).Price);
+            Assert.IsNotNull(result.ReturnedObject);
+            Assert.AreEqual(newPrice.Price, result.ReturnedObject.Prices.Where(x => x.Size == SizeEnum.Small).SingleOrDefault().Price);
         }
 
+
+        private async Task<IEnumerable<IngredientModel>> GetFakeIngredientById(int id)
+        {
+            return expectedIngredients.Where(x => x.Id == id);
+        }
+
+        private async Task<IEnumerable<IngredientModel>> GetFakeIngredientByName(string name)
+        {
+            return expectedIngredients.Where(x => x.Name == name);
+        }
+
+        private async Task<IEnumerable<IngredientModel>> GetFakeIngredients()
+        {
+            return expectedIngredients;
+        }
+
+        private async Task<IEnumerable<PizzaModel>> GetFakePizzas()
+        {
+            PizzaModel pizza = new PizzaModel
+            {
+                Id = 1,
+                Name = "TestPizza",
+                PizzaIngredients = new List<PizzaIngredientsModel>
+                {
+                    new PizzaIngredientsModel
+                    {
+                        PizzaId = 1,
+                        IngredientId = 1,
+                        Ingredient = new IngredientModel
+                        {
+                            Id = 1,
+                            Name = "ham",
+                            Prices = new List<IngredientPriceModel>
+                            {
+                                new IngredientPriceModel
+                                {
+                                    Id = 1,
+                                    IngredientId = 1,
+                                    Size = SizeEnum.Small,
+                                    Price = 2.00M
+                                },
+                                new IngredientPriceModel
+                                {
+                                    Id = 2,
+                                    IngredientId = 1,
+                                    Size = SizeEnum.Big,
+                                    Price = 5.00M
+                                },
+                            }
+                        }
+                    },
+                    new PizzaIngredientsModel
+                    {
+                        PizzaId = 1,
+                        IngredientId = 2,
+                        Ingredient = new IngredientModel
+                        {
+                            Id = 2,
+                            Name = "mushrooms",
+                            Prices = new List<IngredientPriceModel>
+                            {
+                                new IngredientPriceModel
+                                {
+                                    Id = 3,
+                                    IngredientId = 2,
+                                    Size = SizeEnum.Small,
+                                    Price = 3.00M
+                                },
+                                new IngredientPriceModel
+                                {
+                                    Id = 4,
+                                    IngredientId = 2,
+                                    Size = SizeEnum.Big,
+                                    Price = 5.50M
+                                },
+                            }
+                        }
+                    }
+                },
+                PizzaStarters = new List<PizzaStarterModel>
+                {
+                    new PizzaStarterModel
+                    {
+                        PizzaId = 1,
+                        StarterId = 1,
+                        Starter = new StarterModel
+                        {
+                            Id = 1,
+                            Name = "Starter1",
+                            Size = SizeEnum.Small,
+                            Price = 15.00M
+                        }
+                    },
+                    new PizzaStarterModel
+                    {
+                        PizzaId = 1,
+                        StarterId = 2,
+                        Starter = new StarterModel
+                        {
+                            Id = 2,
+                            Name = "Starter2",
+                            Size = SizeEnum.Big,
+                            Price = 25.00M
+                        }
+                    }
+                }
+            };
+            
+            return new List<PizzaModel> { pizza };
+        }
     }
 }
